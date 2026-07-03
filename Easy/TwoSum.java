34266a040924027c662b07be0978bc5a82cba0a5
Virgilio Sánchez <134342661+VirgilioSz@users.noreply.github.com>
2026-07-03 13:41:40 -0600

    TwoSum

---
+++ Easy/TwoSum.java
@@ -0,0 +1,49 @@
+package Easy;
+import java.util.HashMap;
+
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[] { i, j };
+                }
+            }
+        }
+        return new int[] { -1, -1 };
+    }
+
+    public int[] twoSumMap(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            map.put(nums[i], i);
+        }
+       return new int[] { -1, -1 };
+    }
+
+    public static void main(String[] args) {
+        TwoSum solution = new TwoSum();
+
+        // Test case 1
+        int[] nums1 = { 2, 7, 11, 15 };
+        int target1 = 9;
+        int[] result1 = solution.twoSum(nums1, target1);
+        System.out.println("Test 1: [" + result1[0] + ", " + result1[1] + "]"); // Expected: [0, 1]
+
+        // Test case 2
+        int[] nums2 = { 3, 2, 4 };
+        int target2 = 6;
+        int[] result2 = solution.twoSum(nums2, target2);
+        System.out.println("Test 2: [" + result2[0] + ", " + result2[1] + "]"); // Expected: [1, 2]
+
+        // Test case 3
+        int[] nums3 = { 3, 3 };
+        int target3 = 6;
+        int[] result3 = solution.twoSum(nums3, target3);
+        System.out.println("Test 3: [" + result3[0] + ", " + result3[1] + "]"); // Expected: [0, 1]
+    }
+}
