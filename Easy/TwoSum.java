48a6351b614200ee3347789ab2b60e08c47436c0
Virgilio <virgiliosanchez3435@gmail.com>
2026-07-03 13:59:14 -0600

    Problema: TwoSumMap

--- Easy/TwoSum.java
+++ Easy/TwoSum.java
@@ -31,19 +31,19 @@ public class TwoSum {
         // Test case 1
         int[] nums1 = { 2, 7, 11, 15 };
         int target1 = 9;
-        int[] result1 = solution.twoSum(nums1, target1);
+        int[] result1 = solution.twoSumMap(nums1, target1);
         System.out.println("Test 1: [" + result1[0] + ", " + result1[1] + "]"); // Expected: [0, 1]
 
         // Test case 2
         int[] nums2 = { 3, 2, 4 };
         int target2 = 6;
-        int[] result2 = solution.twoSum(nums2, target2);
+        int[] result2 = solution.twoSumMap(nums2, target2);
         System.out.println("Test 2: [" + result2[0] + ", " + result2[1] + "]"); // Expected: [1, 2]
 
         // Test case 3
         int[] nums3 = { 3, 3 };
         int target3 = 6;
-        int[] result3 = solution.twoSum(nums3, target3);
+        int[] result3 = solution.twoSumMap(nums3, target3);
         System.out.println("Test 3: [" + result3[0] + ", " + result3[1] + "]"); // Expected: [0, 1]
     }
 }
